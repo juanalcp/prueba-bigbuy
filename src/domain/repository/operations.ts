3b75b23faa3ec7e9626ccd60062b2f6c994b641b
Juan Alcaraz <juan.arkanoid@gmail.com>
2022-09-24 13:32:51 +0200

    added employees data

---
+++ src/domain/repository/operations.ts
@@ -0,0 +1,5 @@
+import employees from "response/data.json";
+
+export const getEmployees = () => {
+  return employees;
+};
--- src/views/employees/employees.tsx
+++ src/views/employees/employees.tsx
@@ -2,9 +2,13 @@ import FilterForm from "./filter-form";
 import AddEmployeeButton from "./add-employee";
 import { Card } from "components";
 
+import { getEmployees } from "domain/repository/operations";
+
 import "./styles.scss";
 
 const Employees = () => {
+  const employeesData = getEmployees();
+  console.log(employeesData);
   const onFilter = () => {
     console.log(document.forms.namedItem("filter")?.["filter-input"].value);
   };
